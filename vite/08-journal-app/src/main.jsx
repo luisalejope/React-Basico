2de4737e934e572163acd70f95073d1a25dfb869
Luis Alejandro Peña <luisalejandropena425@gmail.com>
2024-04-22 14:42:59 -0500

    journal initialize routes

--- vite/08-journal-app/src/main.jsx
+++ vite/08-journal-app/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './styles.css'
 import { JournalApp } from './JournalApp.jsx'
 
---
+++ vite/08-journal-app/src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+export const AppRouter = () => {
+
+
+    const router = createBrowserRouter([
+        
+    ])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
