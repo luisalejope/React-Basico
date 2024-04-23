c5af79bf9ace20c3e1681198ca64c34bd8682497
Luis Alejandro Peña <luisalejandropena425@gmail.com>
2024-04-22 21:51:30 -0500

    integration with material ui vite journal app

---
+++ vite/08-journal-app/src/theme/AppTheme.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import { ThemeProvider } from "@emotion/react"
+import { CssBaseline } from "@mui/material"
+import { purpleTheme } from './purpleTheme'
+
+export const AppTheme = ({children}) => {
+  return (
+    <ThemeProvider theme={purpleTheme}>
+        <CssBaseline />
+
+        {children}
+    </ThemeProvider>
+  )
+}
+
+
+AppTheme.propTypes = {
+    children: PropTypes.any
+}
+
